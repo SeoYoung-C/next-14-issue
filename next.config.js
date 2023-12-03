e43be8cbd677533e8d0ca00206c958983bd46e61
SeoYoung Choi <seoyoungchoi@SeoYoungui-MacBookPro.local>
2023-12-03 14:26:10 +0900

    fix: 14.0.4-canary.36

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    compiler: {
+        removeConsole: {
+            exclude: ['error', 'info', 'warn'],
+        },
+    },
     experimental: {
         serverComponentsExternalPackages: ['@elastic/ecs-winston-format'],
     }
